cb6a13f53933ae42ce99734cc72173075208aa57
benji6 <benhall22@hotmail.co.uk>
2015-05-27 22:54:37 +0100

    using gulp-connect

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,38 @@
-var autoprefixer = require('gulp-autoprefixer');
-var babel = require("gulp-babel");
-var browserify = require('browserify');
-var buffer = require('vinyl-buffer');
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var minifycss = require('gulp-minify-css');
-var minifyHTML = require('gulp-minify-html');
-var plumber = require('gulp-plumber');
-var sass = require('gulp-sass');
-var source = require('vinyl-source-stream');
-var sourcemaps = require('gulp-sourcemaps');
-var uglify = require("gulp-uglify");
-var watchify = require('watchify');
-var createGalleryData = require('./createGalleryData.js');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require("gulp-babel");
+const browserify = require('browserify');
+const buffer = require('vinyl-buffer');
+const connect = require('gulp-connect');
+const gulp = require('gulp');
+const gutil = require('gulp-util');
+const minifycss = require('gulp-minify-css');
+const minifyHTML = require('gulp-minify-html');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const source = require('vinyl-source-stream');
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require("gulp-uglify");
+const watchify = require('watchify');
+const createGalleryData = require('./createGalleryData.js');
+
+const distPath = "dist/";
+
+gulp.task("connect", function () {
+  connect.server({livereload: true});
+});
+
+gulp.task("reload", function () {
+  gulp.src(distPath).pipe(connect.reload());
+});
 
 gulp.task("html", function () {
   gulp.src('./src/index.html')
     .pipe(minifyHTML())
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest(distPath));
 });
 
 gulp.task("jsDist", function () {
-  var bundler = watchify(browserify('./src/js/main.js', watchify.args));
+  const bundler = watchify(browserify('./src/js/main.js', watchify.args));
 
   bundler.bundle()
     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
@@ -35,7 +46,7 @@ gulp.task("jsDist", function () {
 });
 
 gulp.task("jsDev", function () {
-  var bundler = watchify(browserify('./src/js/main.js', watchify.args));
+  const bundler = watchify(browserify('./src/js/main.js', watchify.args));
 
   bundler.bundle()
     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
@@ -46,7 +57,7 @@ gulp.task("jsDev", function () {
     .pipe(babel())
     .pipe(sourcemaps.write('.'))
     .pipe(plumber.stop())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(distPath));
 });
 
 gulp.task('sass', function () {
@@ -61,17 +72,18 @@ gulp.task('sass', function () {
     }))
     .pipe(minifycss())
     .pipe(plumber.stop())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(distPath));
 });
 
 gulp.task('createGalleryData', createGalleryData);
 
 gulp.task("watch", function () {
-  gulp.start("createGalleryData", "html", "jsDev", "sass");
-  gulp.watch('dist/images/**/*.jpg', ["createGalleryData"]);
+  gulp.start("createGalleryData", "html", "jsDev", "sass", "connect");
+  gulp.watch(distPath + 'images/**/*.jpg', ["createGalleryData"]);
   gulp.watch('src/index.html', ["html"]);
   gulp.watch('src/js/**/*.js', ["jsDev"]);
   gulp.watch('src/sass/style.scss', ["sass"]);
+  gulp.watch(distPath + "*", ["reload"]);
 });
 
 gulp.task("build", ["createGalleryData", "html", "jsDist", "sass"]);
